e61e1671f75b6801cd1867ebf262cc47ff6f1079
reaperes <emerald105@hanmail.net>
2015-06-28 19:27:15 +0900

    add findOneByDateAndTeam

--- src/domain/game/gameservice.js
+++ src/domain/game/gameservice.js
@@ -7,6 +7,32 @@ module.exports = function (sequelize) {
      */
     findAll: function (callback) {
       Game.findAll().then(callback);
+    },
+
+    /**
+     * @param date
+     * @param team {Team} instance
+     * @param callback
+     */
+    findOneByDateAndTeam: function (date, team, callback) {
+      var startDate = new Date(date.getYear(), date.getMonth(), date.getDay(), 0, 0, 0, 0);
+      var endDate = new Date(startDate + 24 * 60 * 60 * 1000 - 1);
+
+      Game.findAll({
+        where: {
+          date: {
+            $lt: startDate,
+            $gt: endDate
+          },
+          $or: [
+            {home: team.id},
+            {away: team.id}
+          ]
+        }
+      }).then(function (rows) {
+        callback(rows[0]);
+      });
+
     }
   }
 };
